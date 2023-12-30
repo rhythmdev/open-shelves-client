d9c132cf8112f4520f45ca60abea6ecf60fd4c74
rhythmdev <rhythmbiswas@gmail.com>
2023-12-30 14:42:50 +0600

    all books filter button issue fixed

--- src/Pages/AllBooks/AllBooks.jsx
+++ src/Pages/AllBooks/AllBooks.jsx
@@ -2,9 +2,12 @@ import { useEffect, useState } from "react";
 import useApiUrl from "../../hooks/useApiUrl";
 import AllBook from "./AllBook";
 
+
 const AllBooks = () => {
 
     const [allBooks, setAllBooks] = useState([]);
+    const [filteredBooks, setFilteredBooks] = useState([]);
+    const [showBooks, setShowBooks] = useState([]);
 
     const apiUrl = useApiUrl();
 
@@ -13,18 +16,53 @@ const AllBooks = () => {
             .then(res => {
                 // console.log(res.data);
                 setAllBooks(res.data)
+                setShowBooks(res.data)
             })
             .catch(err => console.log(err))
     }
         , [apiUrl])
 
+  // get filtered books
+   
+  useEffect(() => {
+    apiUrl.get('/api/filteredBooks')
+    .then(res => {
+        setFilteredBooks(res.data)
+
+    })
+    .catch(error => {
+        console.log(error);
+    })
+  }, [apiUrl])
+
+  const handelAvailableBooks = () => {
+   setShowBooks(filteredBooks)
+  }
+
+  const handelAllBooks = () => {
+    setShowBooks(allBooks)
+  }
+
 
     return (
-        <div className="mt-8">
-            <h2 className="text-2xl font-bold text-center text-gray-600">All Books: {allBooks?.length}</h2>
+        <div className="mt-10">
+           
+            <div>
+                <button onClick={handelAllBooks} className="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-semibold text-gray-800 rounded-lg group bg-gradient-to-br from-pink-500 to-orange-400 group-hover:from-pink-500 group-hover:to-orange-400 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-pink-200 dark:focus:ring-pink-800 mt-3">
+                    <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
+                        All Books 
+                    </span>
+                </button>
+                <button onClick={handelAvailableBooks} className="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-semibold text-gray-800 rounded-lg group bg-gradient-to-br from-pink-500 to-orange-400 group-hover:from-pink-500 group-hover:to-orange-400 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-pink-200 dark:focus:ring-pink-800 mt-3">
+                    <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
+                        Available Books
+                    </span>
+                </button>
+            </div>
+
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 py-10">
                 {
-                    allBooks?.map(allBook => <AllBook key={allBook?._id} allBook={allBook} />)
+                    showBooks?.map(allBook => <AllBook key={allBook?._id} allBook={allBook} />)
                 }
             </div>
         </div>
