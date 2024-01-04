668e1c5404fc894b0adb56fc576b4c795c764572
rhythmdev <rhythmbiswas@gmail.com>
2024-01-04 21:42:09 +0600

    readme file updated

--- README.md
+++ README.md
@@ -1,8 +1,19 @@
-# React + Vite
+# Open Shelves
+#####Borrow, lend, discover. A community library for everyone.
 
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
+## Website Link: [Open Shelves] (https://open-shelves-44471.web.app)
+## Server Link: [Open Shelves] ( https://github.com/Porgramming-Hero-web-course/b8a11-server-side-rhythmdev)
 
-Currently, two official plugins are available:
-
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+###Features
+***Add Book***
+- Registered user can add book from different 4 types of category. 
+***Borrow Book***
+- Any registered user can borrow book form 4 types of different categories. If the books from specif category available then  user can  borrow the book. If the book not available then the user can't borrow the book.
+***Return Book***
+- Registered user can return the book
+***Update Book***
+- Registered user can update any book.
+***Authentication with firebase***
+- For user register and login related task are being used firebase. google login area also available.
+***Jwt***
+- For secure the api Jwt are used with cookies.
--- src/Components/Foooter/Foooter.jsx
+++ src/Components/Foooter/Foooter.jsx
@@ -9,7 +9,7 @@ const Foooter = () => {
         <div className="w-full h-full max-w-screen-xl mx-auto p-4 md:py-8">
           <div className="sm:flex sm:items-center sm:justify-between">
             <Link to="/" className="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse">
-              <img src={logo} className="h-8" alt="Flowbite Logo" />
+              <img src={logo} className="h-8" alt=" Logo" />
               <span className=" font-lobster self-center whitespace-nowrap text-3xl font-bold dark:text-white bg-gradient-to-r from-gra-start via-gra-middle  to-gra-end bg-clip-text text-transparent">Open Shelves</span>
             </Link>
             <ul className="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
@@ -29,9 +29,9 @@ const Foooter = () => {
           </div>
           <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
           <Link to={'/'}>
-          <span className="block text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <a  className="hover:underline">Open Shelves™</a>. All Rights Reserved.</span>
+            <span className="block text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <a className="hover:underline">Open Shelves™</a>. All Rights Reserved.</span>
           </Link>
-        
+
         </div>
       </footer>
 
